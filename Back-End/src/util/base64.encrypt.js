41be2405823e798f845576b279213c86530eeb60
Matheus Ferreira Santos <109228925+Matheus-FSantos@users.noreply.github.com>
2023-10-09 10:15:34 -0300

    feat: add Base64 crypto class

---
+++ Back-End/src/util/base64.encrypt.js
@@ -0,0 +1,22 @@
+import { Base64 as base64 } from "js-base64";
+
+
+export class Base64 {
+
+    encode(string) {
+        return base64.encode(string);
+    }
+
+    decode(string) {
+        return base64.decode(string);
+    }
+
+    matches(expected, encodedString) {
+        if(!encodedString)
+            return false;
+        
+        const decodedString = this.decode(encodedString);
+        return (decodedString === expected) ? true : false;
+    }
+
+}
