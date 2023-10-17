443047c0e52113bb6ccabdef642e35a39d2e46ab
Matheus Ferreira Santos <109228925+Matheus-FSantos@users.noreply.github.com>
2023-10-17 18:32:27 -0300

    refactor: add create and put methods

--- Back-End/src/repository/users.repository.js
+++ Back-End/src/repository/users.repository.js
@@ -28,6 +28,16 @@ export class UsersRepository {
         return await this.findAll();
     }
 
+    async create(user) {
+        query = `INSERT INTO tb_users (id, name, email, password, createdAt, updatedAt) VALUES (UUID(), ?, ?, ?, now(), now())`;
+        await Connection.promise().execute(query, [user.name, user.email, user.password]);
+    }
+
+    async update(user) {
+        query = `UPDATE tb_users SET ${fields[1]} = ?, ${fields[2]} = ?, ${fields[3]} = ?, ${fields[4]} = ? WHERE ${fields[0]} = ?`;
+        await Connection.promise().execute(query, [user.name, user.email, user.password, user.createdAt, user.id]);
+    }
+
     async delete(id) {
         query = `DELETE FROM tb_users WHERE ${fields[0]} = ?`;
         await Connection.promise().execute(query, [id]);    
