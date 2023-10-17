e20d4ea301b2c222018a3e452c2007d8731593ca
Matheus Ferreira Santos <109228925+Matheus-FSantos@users.noreply.github.com>
2023-10-17 18:33:28 -0300

    refactor: add all bussiness roles to the two new methods (create and update)

--- Back-End/src/service/users.service.js
+++ Back-End/src/service/users.service.js
@@ -2,11 +2,17 @@ import { UsersDTO } from "../DTO/users.dto.js";
 import { UsersRepository } from "../repository/users.repository.js";
 import { UserNotFound } from "../error/specialization/UserNotFound.error.js";
 import { Base64 } from "../util/base64.encrypt.js";
+import { UsersValidations } from "../util/users.validation.js";
+import { InvalidFields } from "../error/specialization/InvalidFields.error.js";
+import { DateFormat } from "../util/dateFormat.format.js";
+import { UserAlreadyExists } from "../error/specialization/UserAlreadyExists.error.js";
 
 let decriptedUser = null;
 
 const base64 = new Base64();
 const usersDTO = new UsersDTO();
+const dateFormat = new DateFormat();
+const usersValidation = new UsersValidations();
 const usersRepository = new UsersRepository();
 
 export class UsersService {
@@ -40,6 +46,7 @@ export class UsersService {
     findById(id) {
         return new Promise(async (resolve, reject) => {
             const user = await usersRepository.findById(id);
+            
             if(!user) {
                 reject(new UserNotFound("The method to search for a user was invoked but it was not found in the database!"));
             } else {
@@ -88,6 +95,111 @@ export class UsersService {
         });
     }
 
+    create(user) { 
+        return new Promise(async (resolve, reject) => {
+            const userExist = await this.findAll().then((datas) => {
+                let userExist = false;
+
+                datas.forEach(data => {
+                    console.log(data.email + " :>> " + user.email);
+
+                    if(user.email === data.email) {
+                        console.log(data.email + " dentro :>> " + user.email);
+                        userExist = true;
+                    }
+                });
+
+                return userExist;
+            }).catch((error) => {
+                if(error instanceof UserNotFound)
+                    reject(new UserNotFound(error.message));                
+                else
+                    console.log(error);
+            });
+
+            if(!userExist) {
+                usersValidation.validation(user).then(async (userPromise) => {
+                    const userReadyToSave = {
+                        name: userPromise.name,
+                        email: base64.encode(String(userPromise.email)),
+                        password: base64.encode(String(userPromise.password))
+                    }
+    
+                    await usersRepository.create(userReadyToSave);
+                    resolve();
+                }).catch((error) => {
+                    if(error instanceof InvalidFields)
+                        reject(new InvalidFields(error.message));                
+                    else
+                        console.log(error);
+                });
+            } else {
+                reject(new UserAlreadyExists("There is already a user with this email registered in our system!"));
+            }
+        });
+    }
+
+    update(newUser, id) {
+        return new Promise(async (resolve, reject) => {
+            /* Verifica e-mail */
+            let userExist = await this.findAll()
+                .then((datas) => {
+                    let userExist = false;
+
+                    datas.forEach((data) => {
+                        if(newUser.email === data.email && data.id !== id) {
+                            userExist = true;
+                        }
+                    });
+
+                    return userExist;
+                })
+                .catch((error) => {
+                    if(error instanceof UserNotFound)
+                        reject(new UserNotFound(error.message));                
+                    else
+                        console.log(error);
+            });
+
+            if (!userExist) {
+                const oldUser = await this.findById(id)
+                    .then((data) => {
+                        return data
+                    })
+                    .catch((error) => {
+                        if(error instanceof UserNotFound)
+                            reject(new UserNotFound(error.message));
+                        else
+                            console.log(error);
+                    }
+                );
+
+                /* Verifica os campos */
+                usersValidation.validation(newUser).then(async (userPromise) => {
+                    if(oldUser) {
+                        const userReadyToUpdate = {
+                            id: id,
+                            name: userPromise.name,
+                            email: base64.encode(String(userPromise.email)),
+                            password: base64.encode(String(userPromise.password)),
+                            createdAt: dateFormat.parseDatabaseFormat(oldUser.createdAt)
+                        }
+
+                        await usersRepository.update(userReadyToUpdate);
+                        resolve();
+                    }
+                }).catch((error) => {
+                    if(error instanceof InvalidFields)
+                        reject(new InvalidFields(error.message));                
+                    else
+                        console.log(error);
+                });
+            } else {
+                reject(new UserAlreadyExists("There is already a user with this email registered in our system!"));
+            }
+        });
+    }
+
     delete(id) {
         return new Promise(async (resolve, reject) => {
             await this.findById(id).then(async () => {
@@ -96,6 +208,8 @@ export class UsersService {
             }).catch((error) => {
                 if(error instanceof UserNotFound)
                     reject(new UserNotFound(error.message));                
+                else
+                    console.log(error);
             });
         });
     }
