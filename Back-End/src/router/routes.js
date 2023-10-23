841b7c8c426956304836f1552f4659eab520a3a9
Matheus-FSantos <matheus.fs.contato@gmail.com>
2023-10-23 18:48:18 -0300

    feat: add new routes to album

--- Back-End/src/router/routes.js
+++ Back-End/src/router/routes.js
@@ -1,8 +1,10 @@
 import { Router, json } from "express";
 import { UsersController } from "../controller/users.controller.js";
+import { AlbumsController } from "../controller/albums.controller.js";
 
 const router = Router();
-const usersController = new UsersController(); 
+const usersController = new UsersController();
+const albumsController = new AlbumsController();
 
 router.use(json());
 
@@ -12,5 +14,14 @@ router.get("/users/name-like/:name", (request, response) => usersController.find
 router.post("/users", (request, response) => usersController.create(request, response));
 router.put("/users/:id", (request, response) => usersController.update(request, response));
 router.delete("/users/:id", (request, response) => usersController.delete(request, response));
+
+router.get("/albums/", (request, response) => albumsController.findAll(request, response));
+router.get("/albums/:id", (request, response) => albumsController.findById(request, response));
+router.get("/albums/name-like/:name", (request, response) => albumsController.findByName(request, response));
+router.get("/albums/artist/:artist", (request, response) => albumsController.findByArtist(request, response));
+router.get("/albums/category/:category", (request, response) => albumsController.findByCategory(request, response));
+router.post("/albums", (request, response) => albumsController.create(request, response));
+router.put("/albums/:id", (request, response) => albumsController.update(request, response));
+router.delete("/albums/:id", (request, response) => albumsController.delete(request, response));
 
 export { router };
