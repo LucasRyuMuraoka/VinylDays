394b2f8e99ec51bc75b232ba0a289d7df81525f9
Matheus-FSantos <matheus.fs.contato@gmail.com>
2023-10-23 18:51:14 -0300

    feat: add new 'albums' table

---
+++ Back-End/migrations/V2_database.schema.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE vinyldays;
+
+USE vinyldays;
+
+/* USER TABLE */
+CREATE TABLE tb_users(
+    id VARCHAR(36) PRIMARY KEY,
+    name VARCHAR(45) NOT NULL,
+    email VARCHAR(75) NOT NULL UNIQUE,
+    password VARCHAR(75) NOT NULL, 
+    createdAt TIMESTAMP NOT NULL,
+    updatedAt TIMESTAMP NOT NULL
+);
+
+/* DISCO TABLE */
+CREATE TABLE tb_albums(
+  id VARCHAR(36) PRIMARY KEY,
+  name VARCHAR(45) NOT NULL,
+  artist VARCHAR(60) NOT NULL,
+  price DOUBLE NOT NULL,
+  oldPrice DOUBLE,
+  createdAt TIMESTAMP NOT NULL,
+  updatedAt TIMESTAMP NOT NULL
+);
