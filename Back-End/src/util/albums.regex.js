8f4863fdb09e8dad7a82b6ec28d1436a99bdd230
Matheus-FSantos <matheus.fs.contato@gmail.com>
2023-10-24 19:16:39 -0300

    feat: add regex to albums fields

--- Back-End/src/util/albums.regex.js
+++ Back-End/src/util/albums.regex.js
@@ -1,4 +1,5 @@
 export const __REGEX__ = {
-    ALBUM_NAME: /^(?=.{1,45}$)[^\s].*$/,
-    ARTIST_NAME: /^(?=.{1,60}$)[A-Za-z0-9\s\-_'&]+$/
+    ALBUM_NAME: /^(?=.{4,45}$)[^\s].*$/,
+    ARTIST_NAME: /^(?=.{4,60}$)[A-Za-z0-9\s\-_'&]+$/,
+    CATEGORY_NAME: /^[a-zA-Z0-9\s-]{3,60}$/
 }
